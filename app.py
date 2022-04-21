f1f2e98faf3338c6ef71f32273c6a8f47baaa60e
ivanbahutski <ivanbogutski1995@gmail.com>
2022-04-21 20:37:34 +0200

    Created the PostGreSQL Database Model

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Bz86q1bg@localhost/collectordb'
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = 'data'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, email, height):
+        self.email = email
+        self.height = height
 
 
 @app.route('/')
@@ -10,9 +24,12 @@ def index():
 
 @app.route('/success', methods=['POST'])
 def success():
-    if request.method == ['POST']:
+    if request.method == 'POST':
         email = request.form['email_name']
         height = request.form['height_name']
+        data = Data(email, height)
+        db.session.add(data)
+        db.session.commit()
         return render_template('success.html')
 
 
