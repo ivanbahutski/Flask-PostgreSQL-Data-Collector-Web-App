4de341df7846d0cf22590c5fcd9411bad53653ce
ivanbahutski <ivanbogutski1995@gmail.com>
2022-04-22 15:08:40 +0200

    Added storing user data to db. Display a message if the user exists

--- app.py
+++ app.py
@@ -27,10 +27,13 @@ def success():
     if request.method == 'POST':
         email = request.form['email_name']
         height = request.form['height_name']
-        data = Data(email, height)
-        db.session.add(data)
-        db.session.commit()
-        return render_template('success.html')
+        if db.session.query(Data).filter(Data.email == email).count() == 0:
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('success.html')
+    return render_template('index.html',
+                           text="Seems like we've got something from that email address already!")
 
 
 if __name__ == "__main__":
